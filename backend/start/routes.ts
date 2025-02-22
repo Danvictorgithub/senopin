e13b47db18fe0ffecfb1364ef328b95f9e303848
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-22 12:48:44 +0800

    updated routes with controllers

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -6,11 +6,31 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-
+const AuthController = () => import('#controllers/auth_controller')
+const UsersController = () => import('#controllers/users_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
 router.get('/', async () => {
   return {
-    hello: 'world',
+    message: 'Senopin API v1.0.0 by DV8',
   }
 })
+router
+  .group(() => {
+    router
+      .group(() => {
+        router.post('login', [AuthController, 'login'])
+        router.get('me', [AuthController, 'me']).use(
+          middleware.auth({
+            guards: ['api'],
+          })
+        )
+      })
+      .prefix('auth'),
+      router
+        .resource('users', UsersController)
+        .apiOnly()
+        .use(['index', 'store', 'update', 'destroy'], middleware.allow_development())
+  })
+  .prefix('api')
