0f32887d6124b12b1bfec7dc205946bb165d3d5c
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-22 13:38:04 +0800

    added adonis mailer:init email_verified on user model

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -26,6 +26,9 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column({ serializeAs: null })
   declare password: string
 
+  @column()
+  declare email_verified: boolean
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
