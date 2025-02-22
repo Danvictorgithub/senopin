896147ffcb6a1ae342cdd590d1de451064123aad
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-22 12:48:31 +0800

    created users controller with validator

---
+++ backend/app/controllers/users_controller.ts
@@ -0,0 +1,54 @@
+import User from '#models/user'
+import { createUserValidator } from '#validators/user'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class UsersController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {
+    return User.all()
+  }
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request, response }: HttpContext) {
+    const payload = await request.validateUsing(createUserValidator)
+    const emailCheck = await User.findBy('email', payload.email)
+    if (emailCheck) {
+      return response.badRequest('Email already exists')
+    }
+    const usernameCheck = await User.findBy('username', payload.username)
+    if (usernameCheck) {
+      return response.badRequest('Username already exists')
+    }
+    return User.create(payload)
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {
+    return User.findOrFail(params.id)
+  }
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {
+    const user = await User.findOrFail(params.id)
+    const payload = await request.validateUsing(createUserValidator)
+    user.merge(payload).save()
+    return user
+  }
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {
+    const user = await User.findOrFail(params.id)
+    user.delete()
+    return user
+  }
+}
