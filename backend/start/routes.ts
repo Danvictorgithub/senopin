46bbbadbc8e7067aba84c3a6177b1569f6cf1829
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-02-23 12:26:17 +0800

    added routes to email implementation

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -6,10 +6,13 @@
 | The routes file is used for defining the HTTP routes.
 |
 */
-const AuthController = () => import('#controllers/auth_controller')
-const UsersController = () => import('#controllers/users_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
+import { throttlePasswordReset } from './limiter.js'
+
+const AuthController = () => import('#controllers/auth_controller')
+const UsersController = () => import('#controllers/users_controller')
+const EmailsController = () => import('#controllers/emails_controller')
 
 router.get('/', async () => {
   return {
@@ -18,6 +21,7 @@ router.get('/', async () => {
 })
 router
   .group(() => {
+    // /api/auth
     router
       .group(() => {
         router.post('login', [AuthController, 'login'])
@@ -33,10 +37,27 @@ router
             })
           )
       })
-      .prefix('auth'),
-      router
-        .resource('users', UsersController)
-        .apiOnly()
-        .use(['index', 'store', 'update', 'destroy'], middleware.allow_development())
+      .prefix('auth')
+    // /api/email
+    router
+      .group(() => {
+        router.get('verify-email/:userId/:hash', [EmailsController, 'verifyEmail'])
+        router.post('send-email-verification', [EmailsController, 'sendEmailVerification']).use(
+          middleware.auth({
+            guards: ['api'],
+          })
+        )
+        router
+          .post('send-password-reset', [EmailsController, 'sendPasswordReset'])
+          .use(throttlePasswordReset)
+        router.get('reset-password/:userId/:hash', [EmailsController, 'verifyResetPassword'])
+        router.post('reset-password/:userId/:hash', [EmailsController, 'resetPassword'])
+      })
+      .prefix('email')
+    // /api/users
+    router
+      .resource('users', UsersController)
+      .apiOnly()
+      .use(['index', 'store', 'update', 'destroy'], middleware.allow_development())
   })
   .prefix('api')
